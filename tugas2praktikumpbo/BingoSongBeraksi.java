2beef50bbef4d1f34c0adb5688bfdd8286f4e68c
Garethgareth26 <114641031+Garethgareth26@users.noreply.github.com>
2023-10-18 23:37:49 +0700

    tugas2pbo

---
+++ tugas2praktikumpbo/BingoSongBeraksi.java
@@ -0,0 +1,10 @@
+package tugas2praktikumpbo;
+
+public class BingoSongBeraksi {
+	
+	public static void main(String[] args){
+        BingoSong Tampilkan = new BingoSong();
+        
+        Tampilkan.TampilanBingo();
+}
+}
