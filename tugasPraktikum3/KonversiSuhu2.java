2a59069b87f1e452e8eff9f23b8b83a116f25e44
Garethgareth26 <114641031+Garethgareth26@users.noreply.github.com>
2023-10-25 22:42:32 +0700

    tugas3PraktikumPBO

---
+++ tugasPraktikum3/KonversiSuhu2.java
@@ -0,0 +1,9 @@
+package tugasPraktikum3;
+
+public class KonversiSuhu2 extends KonversiSuhu {
+	public double fahrenheitToReamur(double fahrenheit) {
+        double celsius = (fahrenheit - 32) * 5/9;
+        return celsius * 4/5;
+    }
+
+}
