5f739aa540f81d64c2547389687184e429d0e559
Garethgareth26 <114641031+Garethgareth26@users.noreply.github.com>
2023-10-25 22:39:37 +0700

    Delete KonversiSuhu2.java

--- KonversiSuhu2.java
+++
@@ -1,9 +0,0 @@
-package tugasPraktikum3;
-
-public class KonversiSuhu2 extends KonversiSuhu {
-	public double fahrenheitToReamur(double fahrenheit) {
-        double celsius = (fahrenheit - 32) * 5/9;
-        return celsius * 4/5;
-    }
-
-}
